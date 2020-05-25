a5ffb5dff60889fb8e6f46f75b020e1e70073354
Felix Waage <felix.waage@sap.com>
2020-05-25 18:56:15 +0200

    app.py added

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ image_size = 28 # width and length
 no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
 image_pixels = image_size * image_size
 data_path = "datasets/"
-train_data = np.loadtxt(data_path + "mnist_train.csv", 
-                        delimiter=",")
+
+num_rows = 0
+for line in open("datasets\mnist_train.csv")
+    num_rows += 1
+
 test_data = np.loadtxt(data_path + "mnist_test.csv", 
                        delimiter=",") 
 test_data[:10]
