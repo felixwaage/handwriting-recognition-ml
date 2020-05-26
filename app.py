d4ceeb2b1b42e2f800b39a819e0b9c7e935ae9c9
Fabio Westphal <torheld@gmx.de>
2020-05-26 13:10:40 +0200

    smoothing

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ image_size = 28 # width and length
 no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
 image_pixels = image_size * image_size
 data_path = "datasets/"
-
-num_rows = 0
-for line in open("datasets\mnist_train.csv")
-    num_rows += 1
-
+train_data = np.loadtxt(data_path + "mnist_train.csv", 
+                        delimiter=",")
 test_data = np.loadtxt(data_path + "mnist_test.csv", 
                        delimiter=",") 
-test_data[:10]
+print (test_data[:10])
+test_data[test_data==255]
+print(test_data.shape)
